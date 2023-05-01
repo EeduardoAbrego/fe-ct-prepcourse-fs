d5c4f6fd51f8c4c897842508db8280dc81d4b43c
eduardoabrego11 <eduardoabrego11@gmail.com>
2023-05-01 14:03:35 -0300

    resolucion ej 09

--- M09 Ejercicios Extra/Ejercicios Extra.js
+++ M09 Ejercicios Extra/Ejercicios Extra.js
@@ -5,15 +5,24 @@ function deObjetoAarray(objeto) {
    // Cada elemento del arreglo padre será un nuevo arreglo que contendrá dos elementos.
    // Estos elementos debe ser cada par clave:valor del objeto recibido.
    // [EJEMPLO]: {D: 1, B: 2, C: 3} ---> [['D', 1], ['B', 2], ['C', 3]].
-   // Tu código:
-}
+    return Object.entries(objeto)
+ }
 
 function numberOfCharacters(string) {
    // La función recibe un string. Debes recorrerlo y retornar un objeto donde cada propiedad es una de las
    // letras del string, y su valor es la cantidad de veces que se repite en el string.
    // Las letras deben estar en orden alfabético.
    // [EJEMPLO]: "adsjfdsfsfjsdjfhacabcsbajda" ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 }
-   // Tu código:
+    var obj = {}
+     
+    for ( let letra of string) {
+      if ( obj.hasOwnProperty(letra)) {
+         obj[letra] += 1 } 
+         else {
+            obj[letra] = 1
+         }
+    }
+     return obj
 }
 
 function capToFront(string) {
@@ -21,7 +30,20 @@ function capToFront(string) {
    // Debes enviar todas las letras en mayúscula al comienzo del string.
    // Retornar el string.
    // [EJEMPLO]: soyHENRY ---> HENRYsoy
-   // Tu código:
+    var resultado = ""
+    var mayuscula = ""
+    var minuscula = ""
+     
+    for (let i = 0; i < string.length; i++) {
+      if ( string[i] === string[i].toUpperCase()) {
+         mayuscula+= string[i]
+      }
+      else {
+         minuscula+= string[i]
+      }
+   }
+   resultado = mayuscula+minuscula
+   return resultado
 }
 
 function asAmirror(frase) {
@@ -29,18 +51,36 @@ function asAmirror(frase) {
    // La diferencia es que cada palabra estará escrita al inverso.
    // [EJEMPLO]: "The Henry Challenge is close!"  ---> "ehT yrneH egnellahC si !esolc"
    // Tu código:
+   var listaFrase = frase.split(" ")
+   var fraseFinal = []
+    for (let i = 0; i < listaFrase.length; i++) {
+      let espejo = listaFrase[i].split("").reverse().join("")
+      fraseFinal.push(espejo)
+    }
+    return fraseFinal.join(" ")
 }
 
 function capicua(numero) {
    // Si el número que recibes es capicúa debes retornar el string: "Es capicua".
    // Caso contrario: "No es capicua".
-   // Tu código:
+  if ( numero == numero.toString().split("").reverse().join("")) {
+   return "Es capicua" }
+   else {
+      return "No es capicua" }
 }
 
 function deleteAbc(string) {
    // Tu tarea es eliminar las letras "a", "b" y "c" del string recibido.
    // Retorna el string sin estas letras.
-   // Tu código:
+   let newString = "";
+for ( let i= 0 ; i < string.length ; i ++){
+   if (string[i] === "a" || string[i] === "b" ||  string[i] === "c" )
+   continue;
+   else { newString = newString + string[i]};
+}
+ return newString
+
+
 }
 
 function sortArray(arrayOfStrings) {
@@ -49,6 +89,15 @@ function sortArray(arrayOfStrings) {
    // de la longitud de cada string.
    // [EJEMPLO]: ["You", "are", "beautiful", "looking"]  ---> [“You", "are", "looking", "beautiful"]
    // Tu código:
+   let valor
+   for (let i = 0; i < arrayOfStrings.length - 1; i++) {
+      for (let j =i+1; j < arrayOfStrings.length; j++) {
+         valor = arrayOfStrings[i]
+         if ( arrayOfStrings[i].length > arrayOfStrings[j].length) {
+         arrayOfStrings[i] = arrayOfStrings[j]
+         arrayOfStrings[j] = valor } }
+      }
+      return arrayOfStrings
 }
 
 function buscoInterseccion(array1, array2) {
@@ -57,7 +106,13 @@ function buscoInterseccion(array1, array2) {
    // [EJEMPLO]: [4,2,3] U [1,3,4] = [3,4].
    // Si no tienen elementos en común, retornar un arreglo vacío.
    // [PISTA]: los arreglos no necesariamente tienen la misma longitud.
-   // Tu código:
+   var array3 = []
+     for (let i = 0 ; i < array1.lengtht ; i++) {
+         if (array2.includes(array1[i])) {
+           array3.push(array1[i]) 
+         }
+      }
+   return array3
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
